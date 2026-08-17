85bd0f0780804706bf96913a8d4dc59cc4c74151
Ghislain KIMA <ghiskima@gmail.com>
2026-08-17 20:51:05 +0000

    feat(company): add model company

--- company/models.py
+++ company/models.py
@@ -2,32 +2,15 @@ from django.db import models
 
 
 
-# class Company(models.Model):
-#     name = models.CharField(max_length=150, null=False)
-#     slogan = models.CharField(max_length=255)
-#     description = models.TextField()
-#     sector = models.CharField(max_length=100)
-#     logo_url = models.CharField(max_length=255)
-#     email = models.CharField(max_length=150)
-#     phone = models.CharField(max_length=30)
-#     address = models.TextField()
-#     created_at = models.DateTimeField()
-
-#     def __str__(self) -> str:
-#         return self.name
-
-
-
 class Company(models.Model):
-    id = models.SmallAutoField(primary_key=True, unique=True)
-    name = models.CharField(max_length=150, null=False)
-    slogan = models.CharField(max_length=255, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    sector = models.CharField(max_length=100, blank=True, null=True)
-    logo_url = models.CharField(max_length=255, blank=True, null=True)
-    email = models.CharField(max_length=150, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150)
+    slogan = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    sector = models.CharField(max_length=100, blank=True)
+    logo_url = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(max_length=150, unique=True, blank=True, null=True)
     phone = models.CharField(max_length=30, unique=True, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
+    address = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
